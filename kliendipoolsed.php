6bdf5cc6f837f5a2f340e332e13bcac9d26c9ed3
jyrgenhallast <jurgen.hallast@khk.ee>
2015-09-23 12:39:16 +0300

    {summary} [Deliver #{number}]

---
+++ kliendipoolsed.php
@@ -0,0 +1,18 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Jürgen
+ * Date: 23.09.15
+ * Time: 11:14
+ */
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
