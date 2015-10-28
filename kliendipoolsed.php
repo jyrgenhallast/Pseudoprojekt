1c3b727e5750fa383f5f8813d79c60bae4881dff
jyrgenhallast <jurgen.hallast@khk.ee>
2015-10-28 12:47:56 +0200

    4. Navigeerimatu link [Delivers #103977342]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -2,13 +2,13 @@
 
 
 <!doctype html>
-<html lang="en">
+<html lang="et">
 <head>
     <meta charset="UTF-8">
     <title>Pseudo</title>
 </head>
 <body>
-<p>Pseudo 1</p>
+<p>Pseudo 2</p>
 <button onclick="tereMaailm()">Tere Maailm</button>
 <script>
     function tereMaailm() {
@@ -17,7 +17,7 @@
 </script>
 <br>
 
-<p>Pseudo 2</p>
+<p>Pseudo 3</p>
 <script type="text/javascript">
     function linkAlert(node) {
         return alert("Tere Maailm");
@@ -25,6 +25,18 @@
 </script>
 <a href="http://www.khk.ee" onclick="return linkAlert(this);">Tere Maailm</a>
 <br>
+
+<p>Pseudo 4</p>
+<script type="text/javascript">
+    function linkAlert(node) {
+        return alert("Jaame siia");
+    }
+</script>
+<a href="www.khk.ee" onclick="linkAlert(this); return false;">Jaame siia</a>
+<br>
+
+<p>Pseudo 5</p>
+
 
 </body>
 </html>
