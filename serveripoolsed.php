abf18d2d474f588f7518d38c4bd5c46f48404ea1
jyrgenhallast <jurgen.hallast@khk.ee>
2015-10-29 03:15:24 +0200

    14. Täisarvud [Delivers #103977362]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -4,3 +4,5 @@ print "Tere maailm"."<br>";
 $eesnimi = "Jurgen";
 $perekonnanimi = "Hallast";
 print "$eesnimi $perekonnanimi"."<br>";
+$vanus = 32;
+print "$eesnimi $perekonnanimi ($vanus)"."<br>";
