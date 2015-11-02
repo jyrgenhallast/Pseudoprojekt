a7c33c6e68a0d6ee1f9011b24a35a5f1a4e996ea
jyrgenhallast <jurgen.hallast@khk.ee>
2015-11-02 13:31:17 +0200

    16. Massiivid [Delivers #103977366]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -7,4 +7,10 @@ print "$eesnimi $perekonnanimi"."<br>";
 $vanus = 32;
 print "$eesnimi $perekonnanimi ($vanus)"."<br>";
 $eesnimi_algab_vokaaliga=false;
+$isik = array(
+    "eesnimi" => $eesnimi,
+    "perekonnanimi"=> $perekonnanimi,
+    "vanus"=>$vanus,
 
+);
+print "$isik[eesnimi] $isik[perekonnanimi] ($isik[vanus])";
