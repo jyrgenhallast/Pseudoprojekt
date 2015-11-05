5d5be282e067548f4554addab5bb90d9b1c6a4e2
jyrgenhallast <jurgen.hallast@khk.ee>
2015-11-05 08:52:17 +0200

    20. Floats [Delivers #103977374]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -31,3 +31,8 @@ if ($first[0]=="A" || $first[0]=="E" || $first[0]=="I" || $first[0]=="O" || $fir
     $first[0]=="a" || $first[0]=="e" || $first[0]=="i" || $first[0]=="o" || $first[0]=="u" || $first[0]=="õ" || $first[0]=="ä" || $first[0]=="ö" || $first[0]=="ü") {
     print "<br>"."Nimi algab vokaaliga"."<br>";
 };
+
+$num1= 1.23;
+$num2= 2.34;
+$sum= floatval($num1+$num2);
+print "<br>".$sum. "<br>";
