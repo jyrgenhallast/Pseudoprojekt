c7ecce877427d67e3f70972a6087bf7fda85a93d
jyrgenhallast <jurgen.hallast@khk.ee>
2015-11-05 08:42:32 +0200

    19. Funktsioonid [Delivers #103977372]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -8,7 +8,7 @@ $vanus = 32;
 print "$eesnimi $perekonnanimi ($vanus)"."<br>";
 $eesnimi_algab_vokaaliga=false;
 //$isik = array(
-  //  "eesnimi" => $eesnimi,
+  //  "eesnimi" => $eesnimi,+
     //"perekonnanimi"=> $perekonnanimi,
     //"vanus"=>$vanus,
 //);
@@ -25,3 +25,9 @@ print "$isikObj->eesnimi $isikObj->perekonnanimi ($isikObj->vanus) $isikObj->sug
 if ($eesnimi_algab_vokaaliga){
     print "Eesnimi algab vokaaliga"."<br>";
 }
+$nimiSplit = str_split($eesnimi, 1);
+$first = $nimiSplit[0];
+if ($first[0]=="A" || $first[0]=="E" || $first[0]=="I" || $first[0]=="O" || $first[0]=="U" || $first[0]=="Õ" || $first[0]=="Ä" || $first[0]=="Ö" || $first[0]=="Ü" ||
+    $first[0]=="a" || $first[0]=="e" || $first[0]=="i" || $first[0]=="o" || $first[0]=="u" || $first[0]=="õ" || $first[0]=="ä" || $first[0]=="ö" || $first[0]=="ü") {
+    print "<br>"."Nimi algab vokaaliga"."<br>";
+};
