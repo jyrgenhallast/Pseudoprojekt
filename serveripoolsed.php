7eac42d483b8b344cd2f9b86a109de20d321e3a1
jyrgenhallast <jurgen.hallast@khk.ee>
2015-11-05 07:21:27 +0200

    18. Objektid [Delivers #103977370]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -7,12 +7,20 @@ print "$eesnimi $perekonnanimi"."<br>";
 $vanus = 32;
 print "$eesnimi $perekonnanimi ($vanus)"."<br>";
 $eesnimi_algab_vokaaliga=false;
-$isik = array(
-    "eesnimi" => $eesnimi,
-    "perekonnanimi"=> $perekonnanimi,
-    "vanus"=>$vanus,
-);
-print "$isik[eesnimi] $isik[perekonnanimi] ($isik[vanus])"."<br>";
+//$isik = array(
+  //  "eesnimi" => $eesnimi,
+    //"perekonnanimi"=> $perekonnanimi,
+    //"vanus"=>$vanus,
+//);
+//print "$isik[eesnimi] $isik[perekonnanimi] ($isik[vanus])"."<br>";
+class isik {
+    var $eesnimi = "Jurgen";
+    var $perekonnanimi = "Hallast";
+    var $vanus = 32;
+    var $sugu = "mees";
+}
+$isikObj = new isik;
+print "$isikObj->eesnimi $isikObj->perekonnanimi ($isikObj->vanus) $isikObj->sugu";
 
 if ($eesnimi_algab_vokaaliga){
     print "Eesnimi algab vokaaliga"."<br>";
