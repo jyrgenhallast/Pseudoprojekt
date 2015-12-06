348e037f45973f855a9249feb0e3039e7e0d0fc2
jyrgenhallast <jurgen.hallast@khk.ee>
2015-12-06 20:05:35 +0200

    23. Require [Delivers #103977380]

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+
+if (isset($_GET['page'])) {
+    require $_GET['page'] . ".php";
+}
+?>
+
+
+</body>
+</html>
